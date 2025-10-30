fde809a4cdc959e7002a18833ed4b2fbdf1f03ac
Peppa Pig <rohit.pandey@outlook.com>
2025-10-30 23:44:25 +1100

    added logging interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from './guard/auth.guard';
 import { ExceptionsFilter } from './exception/exceptions.filter';
+import { DetailedLoggingInterceptor } from './interceptor/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalGuards(new AuthGuard());
   app.useGlobalFilters(new ExceptionsFilter());
+  app.useGlobalInterceptors(new DetailedLoggingInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
